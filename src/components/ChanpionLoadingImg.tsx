e24e8a1f4df5716308694e6ce0ab3cd4050d634a
yun8boo <yuma051521@gmail.com>
2021-07-19 23:42:47 +0900

    fix

--- src/components/ChanpionLoadingImg.tsx
+++ src/components/ChanpionLoadingImg.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Image from 'next/image'
 
 interface ChanpionLoadingImgProps {
@@ -6,8 +7,12 @@ interface ChanpionLoadingImgProps {
 }
 
 const ChanpionLoadingImg = ({chanpionId, skin}: ChanpionLoadingImgProps) => {
+  const [width, setwidth] = useState(0)
+  useEffect(() => {
+    setwidth(window.innerWidth);
+  }, [])
   return (
-    <Image layout="fill" key={skin.id} alt="" src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${chanpionId}_${skin.num}.jpg`}/>
+    <Image width={width / 5} height={width / 5 * 1.81818} key={skin.id} alt="" src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${chanpionId}_${skin.num}.jpg`}/>
   )
 }
 
--- src/components/ChanpionSplashImg.tsx
+++ src/components/ChanpionSplashImg.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Image from 'next/image'
 
 interface ChanpionLoadingImgProps {
@@ -6,8 +7,12 @@ interface ChanpionLoadingImgProps {
 }
 
 const ChanpionSplashImg = ({chanpionId, skin}: ChanpionLoadingImgProps) => {
+  const [width, setwidth] = useState(0)
+  useEffect(() => {
+    setwidth(window.innerWidth);
+  }, [])
   return (
-    <Image layout="fill" key={skin.id} alt="" src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${chanpionId}_${skin.num}.jpg`}/>
+    <Image width={width} height={width * 0.5898} key={skin.id} alt="" src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${chanpionId}_${skin.num}.jpg`}/>
   )
 }
 
