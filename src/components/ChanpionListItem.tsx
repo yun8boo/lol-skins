91aa6e61d35958431eb613d327c62a6fbf5ed66c
yun8boo <yuma051521@gmail.com>
2021-07-28 00:02:37 +0900

    fix

--- src/components/ChanpionListItem.tsx
+++ src/components/ChanpionListItem.tsx
@@ -6,7 +6,6 @@ interface ChanpionListItem {
 }
 
 const ChanpionListItem = ({chanpionInfo}: ChanpionListItem) => {
-  console.log(chanpionInfo);
   
   const imagePath = `http://ddragon.leagueoflegends.com/cdn/11.9.1/img/champion/${chanpionInfo.image.full}`;
   return (
